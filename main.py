2726cce8741d2620b4ef3549ab6f6b06695cfd38
Denys-Doskochynskiy <Denys-Doskochynskiy@iONKiD.github.com>
2020-11-19 15:03:54 +0200

    Replace lib"python-telegram-bot" by "aiogram" in project

--- main.py
+++ main.py
@@ -1,110 +1,75 @@
-from telegram import KeyboardButton
-from telegram import ReplyKeyboardMarkup
-from telegram import Update
-from telegram import Bot
-from telegram.ext import Updater
-from telegram.ext import Filters
-from telegram.ext import MessageHandler
-from telegram.ext import CallbackContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-button_plus = "+"
-button_minus = "-"
-button_self = "C"
-button_close = "END"
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
+TOKEN = '1483412678:AAFNfs6V_PlZABFMAZUf712_-xHzuZQ662o'
 
-def messege_handler(update: Update, contex: CallbackContext):
-    file_name = str(update.message.from_user.name)
-    reply_markup = ReplyKeyboardMarkup(
+button_plus = KeyboardButton('+')
+button_minus = KeyboardButton('-')
+button_self = KeyboardButton('Self')
+button_close = KeyboardButton('/send_file')
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
-        keyboard=[[KeyboardButton(text=button_self),
-                   ], [KeyboardButton(text=button_plus), KeyboardButton(text=button_minus),
-                       ], [KeyboardButton(text=button_close), ],
-                  ],
-        resize_keyboard=True,
-    )
-    text = update.message.text
-    try:
-        if (text == button_close):
-            create_write_to_file(file_name,text)
-            update.message.reply_text(
-                text="Свято си скінчило на жуй файл",
-                reply_markup=reply_markup
+markup_big = ReplyKeyboardMarkup()
 
-            )
-        elif (text == button_plus):
-            create_write_to_file(file_name,text)
-            update.message.reply_text(
-                text="Ого ти нажав '+';",
-                reply_markup=reply_markup
-
-            )
-        elif (text == button_self):
-            create_write_to_file(file_name,text)
-            update.message.reply_text(
-                text="Ого ти нажав 'C';",
-                reply_markup=reply_markup
-
-            )
-        elif (text == button_minus):
-            create_write_to_file(file_name,text)
-            update.message.reply_text(
-                text="Ого ти нажав '-';",
-                reply_markup=reply_markup
-
-            )
-        elif (int(text)):
-
-            test = int(text) * int(text)
-            update.message.reply_text(
-                text="Єба то буде в квадраті: " + str(test) + ";",
-                reply_markup=reply_markup
-
-            )
-    except ValueError:
-
-        update.message.reply_text(
-            text='йобаний шашлик то якась хуйня',
-
-            reply_markup=reply_markup
-
-        )
+markup_big.add(
+    button_plus, button_minus, button_self
+)
+markup_big.row(
+    button_close
+)
 
 
-#
-# def do_echo(update: Update, bot: Bot):
-#     text = update.message.text
-#     bot.send_message(
-#         chat_id=update.message.chat_id,
-#         text=text
-#     )
-#     print(text)
-def create_write_to_file(file_name,result):
+# bot.py
+@dp.message_handler(commands=['test'])
+async def process_hi7_command(message: types.Message):
+    await message.reply("Починаєм відмічати таски)", reply_markup=markup_big)
 
-    f = open(file_name+"_terapia.csv", 'a')
+
+@dp.message_handler(commands=['send_file'])
+async def process_file_command(message: types.Message):
+    with open(message.from_user.first_name + "_" + str(message.from_user.id) + ".csv") as file:
+        await message.reply("Ось твій файлик", reply_markup=markup_big)
+        await message.answer_document(file)
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привітик!\nНапиши мені якщо тобі скучно)")
+
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+    print(msg.text)
+    print(msg.from_user.first_name + "_" + str(msg.from_user.id) + ".csv")
+    if (msg.text == "+"):
+        print("plus")
+        print(msg.text)
+        create_write_to_file(msg.from_user.first_name + "_" + str(msg.from_user.id), msg.text)
+
+
+    elif (msg.text == "-"):
+        print("minus")
+        print(msg.text)
+        create_write_to_file(msg.from_user.first_name + "_" + str(msg.from_user.id), msg.text)
+
+    elif (msg.text == "Self"):
+        print("Self")
+        print(msg.text)
+        create_write_to_file(msg.from_user.first_name + "_" + str(msg.from_user.id), msg.text)
+
+
+def create_write_to_file(file_name, result):
+    f = open(file_name + ".csv", 'a', newline='')
 
     f.write(str(result))
-
+    f.write('\n')
     f.close()
 
 
-def start_bot(TOKEN):
-    print("Start this piece of shit")
-
-    updater = Updater(
-
-        token=TOKEN,
-    )
-
-    updater.dispatcher.add_handler(MessageHandler(filters=Filters.all, callback=messege_handler))
-    # messege_echo = MessageHandler(Filters.text, do_echo)
-    #
-    # updater.dispatcher.add_handler(messege_echo)
-    updater.start_polling()
-    updater.idle()
-
-
 if __name__ == '__main__':
-    TOKEN = '1483412678:AAFNfs6V_PlZABFMAZUf712_-xHzuZQ662o'
-
-    start_bot(TOKEN)
+    executor.start_polling(dp)
