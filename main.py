1021cb84c88b8a342166aec10400a7092784bfdd
Denys-Doskochynskiy <Denys-Doskochynskiy@iONKiD.github.com>
2020-11-23 20:53:01 +0200

    Make some fix

--- main.py
+++ main.py
@@ -1,73 +1,136 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
+import pandas as pd
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+import xlwt
+import static_data
+from openpyxl.workbook import Workbook
+import xlsxwriter
+from collections import defaultdict
+
+user_dict = defaultdict(list)
 
 TOKEN = '1483412678:AAFNfs6V_PlZABFMAZUf712_-xHzuZQ662o'
+inline_kb_full = InlineKeyboardMarkup(row_width=3)
+inline_btn_1 = InlineKeyboardButton('+', callback_data='btn1')
+inline_btn_2 = InlineKeyboardButton('-', callback_data='btn2')
+inline_btn_3 = InlineKeyboardButton('Self', callback_data='btn3')
+inline_kb_full.row(inline_btn_1, inline_btn_2, inline_btn_3)
+button_plus = KeyboardButton('/terapia')
 
-button_plus = KeyboardButton('+')
-button_minus = KeyboardButton('-')
-button_self = KeyboardButton('Self')
 button_close = KeyboardButton('/send_file')
 bot = Bot(token=TOKEN)
+list_task = ["test1", "test2", "test3", "test4", "test5", "test6","FINISH"]
 dp = Dispatcher(bot)
-
+list_test = ["Test protocol"]
 markup_big = ReplyKeyboardMarkup()
 
 markup_big.add(
-    button_plus, button_minus, button_self
+    button_plus
 )
 markup_big.row(
     button_close
 )
 
 
+@dp.callback_query_handler()
+async def process_callback_kb1btn1(callback_query: types.CallbackQuery):
+    code = callback_query.data[-1]
+    test = len(list_task) + 1
+    try:
+
+        if len(user_dict[callback_query.from_user.first_name]) < test:
+
+            await bot.send_message(callback_query.from_user.id,
+                                   list_task[len(user_dict[callback_query.from_user.first_name])+1],
+                                   reply_markup=inline_kb_full)
+            print(len(user_dict[callback_query.from_user.first_name]))
+
+            if code.isdigit():
+                code = int(code)
+            if code == 1:
+                user_dict[callback_query.from_user.first_name].append("+")
+
+
+                await bot.answer_callback_query(callback_query.id)
+
+                # await bot.send_message(callback_query.from_user.id, f'Ти нажав +')
+
+            elif code == 2:
+
+                list_test.append("-")
+                await bot.answer_callback_query(callback_query.id)
+                user_dict[callback_query.from_user.first_name].append("-")
+                # await bot.send_message(callback_query.from_user.id, f'Ти нажав -')
+            elif code == 3:
+                list_test.append("Self")
+                await bot.answer_callback_query(callback_query.id)
+
+                user_dict[callback_query.from_user.first_name].append("Self")
+                # await bot.send_message(callback_query.from_user.id, f'Ти нажав Self')
+
+    except IndexError:
+
+        await bot.send_message(callback_query.from_user.id, f'Достаааааааа')
+        print(user_dict)
+
+
 # bot.py
-@dp.message_handler(commands=['test'])
+@dp.message_handler(commands=['terapia'])
 async def process_hi7_command(message: types.Message):
-    await message.reply("Починаєм відмічати таски)", reply_markup=markup_big)
+    await message.reply(list_task[len(user_dict[message.from_user.first_name])], reply_markup=inline_kb_full)
 
 
 @dp.message_handler(commands=['send_file'])
 async def process_file_command(message: types.Message):
-    with open(message.from_user.first_name + "_" + str(message.from_user.id) + ".csv") as file:
+    # write_to_file(message.from_user.first_name + "_" + str(message.from_user.id))
+    create_write_to_file(message.from_user.first_name)
+    with open(message.from_user.first_name  + ".csv") as file:
         await message.reply("Ось твій файлик", reply_markup=markup_big)
         await message.answer_document(file)
 
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Привітик!\nНапиши мені якщо тобі скучно)")
+    await message.reply("Привітик!\nНапиши мені якщо тобі скучно)", reply_markup=markup_big)
 
 
-@dp.message_handler()
-async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id, msg.text)
-    print(msg.text)
-    print(msg.from_user.first_name + "_" + str(msg.from_user.id) + ".csv")
-    if (msg.text == "+"):
-        print("plus")
-        print(msg.text)
-        create_write_to_file(msg.from_user.first_name + "_" + str(msg.from_user.id), msg.text)
+#
+# @dp.message_handler()
+# async def echo_message(msg: types.Message):
+#     await bot.send_message(msg.from_user.id, msg.text)
+#     print(msg.text)
+#     print(msg.from_user.first_name + "_" + str(msg.from_user.id) + ".csv")
+#     if (msg.text == "+"):
+#         print("plus")
+#         print(msg.text)
+#
+#         list_test.append(msg.text)
+#         # create_write_to_file(msg.from_user.first_name + "_" + str(msg.from_user.id), msg.text)
+#
+#
+#     elif (msg.text == "-"):
+#         list_test.append(msg.text)
+#         print("minus")
+#         print(msg.text)
+#         user_dict[msg.from_user.first_name].append(msg.text)
+#         print(user_dict)
+#         # create_write_to_file(msg.from_user.first_name + "_" + str(msg.from_user.id), msg.text)
+#
+#     elif (msg.text == "Self"):
+#         list_test.append(msg.text)
+#         print("Self")
+#         print(msg.text)
+#     # create_write_to_file(msg.from_user.first_name + "_" + str(msg.from_user.id), msg.text)
+#
 
-
-    elif (msg.text == "-"):
-        print("minus")
-        print(msg.text)
-        create_write_to_file(msg.from_user.first_name + "_" + str(msg.from_user.id), msg.text)
-
-    elif (msg.text == "Self"):
-        print("Self")
-        print(msg.text)
-        create_write_to_file(msg.from_user.first_name + "_" + str(msg.from_user.id), msg.text)
-
-
-def create_write_to_file(file_name, result):
-    f = open(file_name + ".csv", 'a', newline='')
-
-    f.write(str(result))
+def create_write_to_file(file_name):
+    f = open(file_name + ".csv", 'a+', newline='')
+    f.write("Iteration")
     f.write('\n')
+    for i in user_dict[file_name]:
+        f.write(str(i) + ",")
     f.close()
 
 
